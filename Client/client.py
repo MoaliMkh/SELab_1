2b2c447f54606c8db9286266a504db49a04442e4
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2023-07-12 20:50:56 +0330

    Complete register feature

--- Client/client.py
+++ Client/client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 
 HOST, PORT = 'localhost', 8080
@@ -7,8 +8,13 @@ server_address = (HOST, PORT)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
-message = input('Enter your Message:\n')
-print(f'Sending:\n{message}')
-client_socket.send(message.encode())
-response, _ = client_socket.recvfrom(1024)
-print(response.decode())
+while True:
+    command = input('Enter your Message:\n')
+    print(f'Sending:\n{command}')
+    
+    command = command.split(' ')
+    if command[0] == 'signup' and len(command) == 3:
+        message_to_send = json.dumps({'type': 'signup', 'username': command[1], 'password': command[2]})
+    client_socket.send(message_to_send.encode())
+    response, _ = client_socket.recvfrom(1024)
+    print(f'Receiving:\n{response.decode()}')
--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,31 @@
 import socket
 import threading
+import json
 
 
 def handle_client(connection_sock):
     while True:
-        print(f'Receiving: {connection_sock.recv(1024).decode()}')
+        command = json.loads(connection_sock.recv(1024).decode())
+        print(f'Receiving: {command}')
+        if command['type'] == 'signup':
+            response = answer_signup(command['username'], command['password'])
 
+
+def answer_signup(username, password):
+    for user in all_users['Users']:
+        if user['username'] == username:
+            message = {'type': 'ERROR', 'message': 'Username is used'}
+            break
+    else:
+        all_users['Users'].append({'username': username, 'password': password})
+        with open('Server/Users.txt', 'w') as file:
+            file.write(json.dumps(all_users, indent=4))
+        message = {'type': 'OK', 'message': 'Signup Successful'}
+    return json.dumps(message)
+
+
+with open('Server/Users.txt', 'r') as file:
+    all_users = json.loads(file.read())
 
 HOST, PORT = 'localhost', 8080
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
