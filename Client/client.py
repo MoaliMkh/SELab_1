aabd2b8cc360af3339ba23dbe1c43c010349a872
MohammadAli MohammadKhani <mohammadali1379@gmail.com>
2023-07-12 20:56:40 +0330

    Complete login process

--- Client/client.py
+++ Client/client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 
 HOST, PORT = 'localhost', 8080
@@ -7,8 +8,16 @@ server_address = (HOST, PORT)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
-message = input('Enter your Message:\n')
-print(f'Sending:\n{message}')
-client_socket.send(message.encode())
-response, _ = client_socket.recvfrom(1024)
-print(response.decode())
+while True:
+    command = input('Enter your Message:\n').lower()
+    command_parts = command.split(' ')
+    if command_parts[0] == 'login' and len(command_parts) == 3:
+        message_to_send = {'type': 'login', 'username': command_parts[1], 'password': command_parts[2], }
+        print(f'Sending:\n{json.dumps(message_to_send)}')
+        client_socket.send(json.dumps(message_to_send).encode())
+        response, _ = client_socket.recvfrom(1024)
+        print(response.decode())
+
+    else:
+        print('Invalid Command')
+        continue
--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,46 @@
 import socket
 import threading
+import json
 
 
 def handle_client(connection_sock):
+    current_user = None
+
+
     while True:
+        encrypted_message = connection_sock.recv(1024)
+        decrypted_message = encrypted_message
+
+        command = decrypted_message.decode()
+        command_dict = json.loads(command)
         print(f'Receiving: {connection_sock.recv(1024).decode()}')
+
+        response = ''
+
+        if command_dict['type'] == 'login':
+            response, current_user = answer_login(command_dict['username'], command_dict['password'], current_user)
+        
+
+        print(f'Sending:\n{response}')
+        connection_sock.send(response.encode())
+
+
+def answer_login(username, password, current_user):
+    if current_user is not None:
+        message = {'type': 'ERROR', 'message': 'Exit Required'}
+    else:
+        for user in all_users['Users']:
+            if user['username'] == username:
+                if user['password'] == password:
+                    message = {'type': 'OK', 'message': 'Login Successful'}
+                    current_user = username
+                    online_users.append(username)
+                else:
+                    message = {'type': 'ERROR', 'message': 'Password is not Correct'}
+                break
+        else:
+            message = {'type': 'ERROR', 'message': 'Username not Found'}
+    return json.dumps(message), current_user
 
 
 HOST, PORT = 'localhost', 8080
@@ -12,6 +48,10 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (HOST, PORT)
 server_socket.bind(server_address)
 server_socket.listen(1)
+
+with open('Server/Users.txt', 'r') as file:
+    all_users = json.loads(file.read())
+online_users = []
 
 print('Server Listening...')
 while True:
